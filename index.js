e7ac10c52157b295b6caf21298e23cff0ae97c60
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-07-17 11:08:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+let drivers = ['Michael', 'Mary', 'Julius', 'Mark', 'Ruth']
+
+
+const returnFirstTwoDrivers =function(drivers) {
+const newDrivers = drivers.slice(0,2);
+return newDrivers;
+}
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function(drivers){
+    const lastDrivers = drivers.slice(-2);
+    return lastDrivers;
+}
+returnLastTwoDrivers(drivers);
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function (multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  };
+  
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, selectorFunction) {
+    return selectorFunction(drivers);
+  };
